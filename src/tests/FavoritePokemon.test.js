8ab79072620cfb0814d4c3e67c273ac25b1b4f27
Maria Gabriela Galindo <gabi_who@live.cm>
2022-11-22 18:20:49 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Teste o componente FavoritePokemon', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const message = screen.getByText('No favorite Pokémon found');
+    expect(message).toBeInTheDocument();
+  });
+});
