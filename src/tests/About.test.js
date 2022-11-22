92cc88a2b31bdd954edbe19c6ef9b74bc2899bcd
Maria Gabriela Galindo <gabi_who@live.cm>
2022-11-22 17:41:56 -0300

    Adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+describe('Teste o componente About', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const headingH2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+
+    expect(headingH2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraph1 = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon');
+    expect(paragraph1).toBeInTheDocument();
+
+    const paragraph2 = screen.getByText('One can filter Pokémon by type, and see more details for each one of them');
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém uma imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const imagePokedex = screen.getByRole('img');
+    expect(imagePokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
