de951902f8875de4fd9957d26e5ff402cb1391d7
Maria Gabriela Galindo <gabi_who@live.cm>
2022-11-23 15:35:46 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste componenet NotFound', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const message = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(message).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const imagePikachu = screen.getByRole('img');
+    expect(imagePikachu).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
